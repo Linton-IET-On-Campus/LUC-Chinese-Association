671f9d7aaf8fe78ac6d961e58a52f0751a5ee813
Jehovah Yii Zui Hon (Hovah) <31881261+hovahyii@users.noreply.github.com>
2020-08-14 19:30:14 +0800

    created main/aboutus components

--- chinese-association/src/app/app.module.ts
+++ chinese-association/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutusComponent } from './main/aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule
