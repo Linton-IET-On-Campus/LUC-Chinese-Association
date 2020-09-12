61d0bd00535a3d07db014ea659b15df03cdd1ff7
Tayjer <tayjer@student.ktgeducation.edu.my>
2020-09-12 19:38:40 +0800

    add Card

--- luc-chinese-association/src/App.js
+++ luc-chinese-association/src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Footer from "./components/Footer/Footer";
-// import Navbar from "./components/navbar.component";
-// import Event from "./components/events.component";
-import Navbar from "./components/Navbar/Navbar";
-import Slider from "./components/Slider/Slider";
+import Events from "./pages/Events/Events";
 
 function App() {
     return (
-        <div className="container">
-        <Navbar />
-        <Slider />
-        <hr />
-        <Footer />
-        
-        </div>
+        <Events />
     );
 }
 
