666506b76a078206cc64e059dca9a7926288c3c4
Jehovah Yii Zui Hon (Hovah) <31881261+hovahyii@users.noreply.github.com>
2020-09-12 21:35:45 +0800

    update events

--- luc-chinese-association/src/App.js
+++ luc-chinese-association/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Footer from "./components/Footer/Footer";
 // import Navbar from "./components/navbar.component";
 // import Event from "./components/events.component";
 import Navbar from "./components/Navbar/Navbar";
 import Slider from "./components/Slider/Slider";
+import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
